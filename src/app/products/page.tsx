37439633f56e187adad1f531968efb52ccbf561d
Ludmilla Vaz <ludmillaslvaz@gmail.com>
2025-02-19 16:18:45 -0300

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
     return <div className="p-5 border border-red-500 rounded-xl">
-        <h1 className="text-red-500">Products Page</h1>;
+        <h1 className="text-red-500">Products Page</h1>
         <Button>FSW 7.0</Button>
         <Input placeholder="Bora fechar esse projeto!"/>
     </div>
